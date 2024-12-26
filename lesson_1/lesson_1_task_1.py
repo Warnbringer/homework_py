f95fd53abe721ac881d05b35ac21061c75048a60
user <warnbringer@gmail.com>
2024-12-26 14:08:49 +0600

    My first commit

--- lesson_1/lesson_1_task_1.py
+++ lesson_1/lesson_1_task_1.py
@@ -1,1 +1,2 @@
-print("Hello")
+my_name = "Roman"
+print(my_name)
---
+++ lesson_1/lesson_1_task_5.py
@@ -0,0 +1,25 @@
+def print_number(num) :
+    print(num)
+
+for _ in range(1) :
+    print_number('8')
+    for _ in range(1):
+        print_number('8')
+        for _ in range(1):
+            print_number('0')
+            for _ in range(1):
+                print_number('0')
+                for _ in range(1):
+                    print_number('5')
+                    for _ in range(1):
+                        print_number('5')
+                        for _ in range(1):
+                            print_number('5')
+                            for _ in range(1):
+                                print_number('3')
+                                for _ in range(1):
+                                    print_number('5')
+                                    for _ in range(1):
+                                        print_number('3')
+                                        for _ in range(1):
+                                            print_number('5')
