bad7802e0d6b6303aa3cd41576848bae2bb6f166
Роман <warnbringer@gmail.com>
2025-01-21 13:46:34 +0600

    # ДЗ-4
    # Поправил по стилю PEP8.

--- lesson_4/test_string_utils.py
+++ lesson_4/test_string_utils.py
@@ -1,54 +1,62 @@
 import pytest
 from StringUtils import StringUtils
 
+
 @pytest.fixture
 def string_utils():
     return StringUtils()
+
 
 @pytest.mark.parametrize("input_str, expected_output", [
     ("skypro", "Skypro"),
     ("SKYPRO", "Skypro"),
     ("hi hi", "Hi hi"),
 ])
-def test_capitalize_positive(string_utils, input_str, expected_output):
+def test_capitalize_pos(string_utils, input_str, expected_output):
     assert string_utils.capitilize(input_str) == expected_output
+
 
 @pytest.mark.parametrize("input_str, expected_output", [
     (" ", " "),
     ("", ""),
     ("123", "123"),
 ])
-def test_capitalize_negative(string_utils, input_str, expected_output):
+def test_capitalize_neg(string_utils, input_str, expected_output):
     assert string_utils.capitilize(input_str) == expected_output
+
 
 @pytest.mark.parametrize("input_str, expected_output", [
     ("   skypro   ", "skypro"),
     ("skypro", "skypro"),
     ("   ", ""),
 ])
-def test_trim_positive(string_utils, input_str, expected_output):
+def test_trim_pos(string_utils, input_str, expected_output):
     assert string_utils.trim(input_str) == expected_output
+
 
 @pytest.mark.parametrize("input_str, expected_output", [
     ("skypro", "skypro"),
     ("", ""),
 ])
-def test_trim_negative(string_utils, input_str, expected_output):
+def test_trim_neg(string_utils, input_str, expected_output):
     assert string_utils.trim(input_str) == expected_output
+
 
 @pytest.mark.parametrize("input_str, expected_output", [
     ("a,b,c", ["a", "b", "c"]),
     ("a b c", ["a", "b", "c"]),
     ("", []),
 ])
-def test_to_list_positive(string_utils, input_str, expected_output):
+def test_to_list_pos(string_utils, input_str, expected_output):
     assert string_utils.to_list(input_str) == expected_output
+
 
 @pytest.mark.parametrize("input_str, delimiter, expected_output", [
     ("a,b,c", ";", ["a,b,c"]),
 ])
-def test_to_list_negative(string_utils, input_str, delimiter, expected_output):
+def test_to_list_neg(string_utils, input_str, delimiter, expected_output):
     assert string_utils.to_list(input_str, delimiter) == expected_output
+
 
 @pytest.mark.parametrize("input_str, char, expected_output", [
     ("SkyPro", "S", True),
@@ -56,6 +64,7 @@ def test_to_list_negative(string_utils, input_str, delimiter, expected_output):
 ])
 def test_contains(string_utils, input_str, char, expected_output):
     assert string_utils.contains(input_str, char) == expected_output
+
 
 @pytest.mark.parametrize("input_str, char, expected_output", [
     ("SkyPro", "r", "SkyPo"),
@@ -65,6 +74,7 @@ def test_contains(string_utils, input_str, char, expected_output):
 def test_delete_symbol(string_utils, input_str, char, expected_output):
     assert string_utils.delete_symbol(input_str, char) == expected_output
 
+
 @pytest.mark.parametrize("input_str, char, expected_output", [
     ("SkyPro", "S", True),
     ("SkyPro", "P", False),
@@ -72,6 +82,7 @@ def test_delete_symbol(string_utils, input_str, char, expected_output):
 ])
 def test_starts_with(string_utils, input_str, char, expected_output):
     assert string_utils.starts_with(input_str, char) == expected_output
+
 
 @pytest.mark.parametrize("input_str, char, expected_output", [
     ("SkyPro", "o", True),
@@ -81,6 +92,7 @@ def test_starts_with(string_utils, input_str, char, expected_output):
 def test_end_with(string_utils, input_str, char, expected_output):
     assert string_utils.end_with(input_str, char) == expected_output
 
+
 @pytest.mark.parametrize("input_str, expected_output", [
     ("", True),
     ("  ", True),
@@ -89,6 +101,7 @@ def test_end_with(string_utils, input_str, char, expected_output):
 def test_is_empty(string_utils, input_str, expected_output):
     assert string_utils.is_empty(input_str) == expected_output
 
+
 @pytest.mark.parametrize("input_list, expected_output", [
     ([1, 2, 3, 4], "1, 2, 3, 4"),
     (["Sky", "Pro"], "Sky, Pro"),
@@ -96,6 +109,3 @@ def test_is_empty(string_utils, input_str, expected_output):
 ])
 def test_list_to_string(string_utils, input_list, expected_output):
     assert string_utils.list_to_string(input_list) == expected_output
-
-
-
