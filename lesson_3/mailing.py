c4e159d36134862dd2d42b6713efba575bf2e70d
Warnbringer <131276160+Warnbringer@users.noreply.github.com>
2025-02-21 12:16:12 +0600

    Delete lesson_3 directory

--- lesson_3/mailing.py
+++
@@ -1,8 +0,0 @@
-from address import Address
-
-class Mailing:
-    def __init__(self, to_address, from_address, cost, track):
-        self.to_address = to_address  # тип данных Address
-        self.from_address = from_address  # тип данных Address
-        self.cost = cost  # тип данных число
-        self.track = track  # тип данных строка
